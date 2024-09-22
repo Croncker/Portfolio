0684eef66cfbbef242f0964b278a723bb58f5f66
Lucca Van Soest <luccavansoest@gmail.com>
2024-09-22 20:12:46 +0200

    fixing project slider

--- js/main.js
+++ js/main.js
@@ -17,3 +17,40 @@ hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('nav-active');
     hamburger.classList.toggle('toggle');
 });
+
+
+
+
+let dots = document.querySelectorAll('.dot');
+let projects = document.querySelectorAll('.projects-info');
+
+function currentSlide(slideIndex) {
+    projects.forEach((project) => {
+        project.style.display = 'none';
+        project.classList.remove('active');
+    });
+
+    if (slideIndex === 1) {
+        for (let i = 0; i < 3; i++) {
+            projects[i].style.display = 'block';
+            projects[i].classList.add('active');
+        }
+    } else if (slideIndex === 2) {
+        for (let i = 3; i < 6; i++) {
+            projects[i].style.display = 'block';
+            projects[i].classList.add('active');
+        }
+    } else if (slideIndex === 3) {
+        for (let i = 6; i < 9; i++) {
+            projects[i].style.display = 'block';
+            projects[i].classList.add('active');
+        }
+    }
+
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[slideIndex - 1].classList.add('active');
+}
+
+currentSlide(1);
+
+
